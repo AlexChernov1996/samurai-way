3853828cfaaba1fc9c54a078143111f9d8ed869c
Alexandr Chernov <vonrehcxela@gmail.com>
2022-08-02 22:11:46 +0300

    Login form. Redux Form

--- src/Components/Login/LoginPage.tsx
+++ src/Components/Login/LoginPage.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import {Field, InjectedFormProps, reduxForm} from "redux-form";
+type FormDataType = {
+    login:string
+    password:string
+    rememberMe:boolean
+}
+const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+    return <form onSubmit={() => props.handleSubmit}>
+        <div>
+            <Field component="input" name='login' placeholder={'login'}/></div>
+        <div>
+            <Field component="input" name='password' placeholder={'password'}/>
+        </div>
+        <div>
+            <Field component="input" name='rememberMe' type="checkbox"/>
+        </div>
+        <button>Login</button>
+    </form>
+}
+const LoginReduxForm = reduxForm<FormDataType>({form: 'login'}) (LoginForm)
 
 const LoginPage = () => {
     return (
         <div>
             <h1>Login</h1>
-        </div>
-    );
+            <LoginReduxForm/>
+        </div>);
 };
 
-export default LoginPage;
+export default LoginPage
--- src/state/store.ts
+++ src/state/store.ts
@@ -3,16 +3,18 @@ import {dialogsReducer} from "./dialogsReducer";
 import {profileReducer} from "./profileReducer";
 import {usersReducer} from "./usersReducer";
 import {authReducer} from "./authReducer";
+import {reducer as formReducer} from "redux-form"
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
-    dialogsPage:dialogsReducer,
-    profilePage:profileReducer,
-    usersPage:usersReducer,
-    auth:authReducer
+    dialogsPage: dialogsReducer,
+    profilePage: profileReducer,
+    usersPage: usersReducer,
+    auth: authReducer,
+    form: formReducer
 })
 
-export const store = createStore(rootReducer,applyMiddleware(thunk))
+export const store = createStore(rootReducer, applyMiddleware(thunk))
 export type AppStateType = ReturnType<typeof rootReducer>
 //@ts-ignore
 window.store = store
